d4161562faa71c8ee94d2738a152ceb559ad4c19
Roniel Bernas <roniel.bernas@crmworksa.asia>
2017-04-03 22:50:33 +0800

    application state can be both simple and/or complex (this depends if there are many objects inside the state) the state is always represented as a single object, that will serve as the single source of truth

---
+++ 00-introduction/01-single-source-of-truth.js
@@ -0,0 +1,48 @@
+const projectLists = [
+    {
+        id: 1,
+        project: 'Space X',
+        goal: 1232.121,
+        funded: false
+    },
+    {
+        id: 2,
+        project: 'Cleaning Flint Water',
+        goal: 5000.12,
+        funded: false
+    },
+    {
+        id: 3,
+        project: 'Rebuild Amazon Forest',
+        goal: 310.22,
+        funded: false
+    }
+]
+
+
+
+const uberDriver = {
+    profile: {
+        id: 10001,
+        name: 'Jimmy Ho',
+        email: 'jimmy-ho@yahoo.com',
+        rating: 4.5
+    },
+
+    passengersNearby:[],
+
+    notifications: [],
+
+    completedRights: [],
+
+    ratings: [
+        {
+            customerId: 50197,
+            rating: 4
+        },
+        {
+            customerId: 50013,
+            rating: 5
+        }
+    ]
+}
