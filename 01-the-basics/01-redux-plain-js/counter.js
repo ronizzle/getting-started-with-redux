970c24130e5bda8533d208b28eafde19b9eb929b
Roniel Bernas <roniel.bernas@crmworksa.asia>
2017-04-06 21:57:06 +0800

    started redux implementation

---
+++ 01-the-basics/01-redux-plain-js/counter.js
@@ -0,0 +1,16 @@
+// REDUCER
+function counter(currentState, action) {
+    switch(action.type) {
+        case 'ADD':
+            break;
+
+        default:
+            break;
+    }
+}
+
+
+var store = Redux.createStore(counter(counter))
+
+// ACTIONS
+{type: 'ADD'}
