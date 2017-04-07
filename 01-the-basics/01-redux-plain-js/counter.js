2ec2714577a14f541f481bf0bd05c3e8ecd1fcbc
Roniel Bernas <roniel.bernas@crmworksa.asia>
2017-04-07 15:50:52 +0800

    example exercise of using states, reducer, pure functions, dispatching a store in redux

--- 01-the-basics/01-redux-plain-js/counter.js
+++ 01-the-basics/01-redux-plain-js/counter.js
@@ -1,16 +1,55 @@
 // REDUCER
 function counter(currentState, action) {
+
+    var nextState = {
+        count: currentState.count
+    }
+
     switch(action.type) {
         case 'ADD':
+            nextState.count = currentState.count + 1
+            return nextState
+            break;
+
+        case 'MINUS':
+            nextState.count = currentState.count -1
+            return nextState
+            break;
+
+        case 'RESET':
+            nextState.count = 0
+            return nextState
             break;
 
         default:
+            return currentState
             break;
     }
 }
 
+var counterEl = document.getElementById('counter');
 
-var store = Redux.createStore(counter(counter))
+var state = {
+    count: 0
+};
 
-// ACTIONS
-{type: 'ADD'}
+function render() {
+    var state = store.getState();
+    counterEl.innerHTML = state.count.toString()
+}
+var store = Redux.createStore(counter, state)
+store.subscribe(render)
+
+
+document.getElementById('add').addEventListener('click', function() {
+    store.dispatch({type: 'ADD'})
+})
+
+document.getElementById('minus').addEventListener('click', function() {
+    store.dispatch({type: 'MINUS'})
+})
+
+document.getElementById('reset').addEventListener('click', function() {
+    store.dispatch({type: 'RESET'})
+})
+
